7348e8c3050db941c4e0a134c1a04d4dfc251e79
roloaltas-byte <roloaltas@proton.me>
2026-08-24 17:42:56 +0300

    Registraciju larpas baigtas turbut.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Depends, Response
+from sqlalchemy.orm import Session
+from app.database import get_db
 from app.api.registration import RegistrationRequest
+from app.models.models import User
 
 app = FastAPI(
     title="Mesh API",
@@ -11,5 +14,20 @@ async def root():
     return {"message": "Mesh API is running"}
 
 @app.post("/register/")
-async def registration(username, email, password ):
-    return {"username": username, "email": email, "password": password}
+async def registration(username, email, password, registration: RegistrationRequest, db: Session = Depends(get_db)):
+
+    db_users = User(   
+        username = registration.username,
+        email = registration.email,
+        password_hash = registration.password,
+    )
+    db.add(db_users)
+    db.commit()
+    db.refresh(db_users)
+    db.close()
+
+    return {
+            "username": registration.username,
+            "email": registration.email,
+            "password": registration.password
+            }
